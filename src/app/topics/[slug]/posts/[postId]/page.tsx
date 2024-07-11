c0fb1b017dc0e514d4e01df41a60bb374d9a2889
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-11 13:26:57 +0530

    add loading skeleton

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,8 @@ import PostShow from "@/components/posts/PostShow";
 import CommentCreateForm from "@/components/comments/CommentCreateForm";
 import CommentList from "@/components/comments/CommentList";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
+import PostShowLoading from "@/components/posts/PostShowLoading";
 
 interface PostShowPageProps {
   params: {
@@ -19,7 +21,7 @@ const PostShowPage = ({params}: PostShowPageProps) => {
     return (
         <div className="space-y-3">
         <Link className="underline decoration-solid" href={paths.topicShow(slug)}>{'< '} Back to {slug}</Link>
-        <PostShow postId={postId} />
+        <Suspense fallback={<PostShowLoading />}><PostShow postId={postId} /></Suspense>
         <CommentCreateForm postId={postId} startOpen />
         <CommentList postId={postId} />
         </div>
---
+++ src/components/posts/PostShowLoading.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from "@nextui-org/react";
+
+const PostShowLoading = () => {
+    return (
+        <div className="m-4">
+            <div className="my-2">
+                <Skeleton className="h-8 w-48"/>
+            </div>
+            <div className="p-4 border rounded space-y-2">
+                <Skeleton className="h-6 w-32"/>
+                <Skeleton className="h-6 w-32"/>
+                <Skeleton className="h-6 w-32"/>
+            </div>
+        </div>
+    );
+}
+
+export default PostShowLoading;
